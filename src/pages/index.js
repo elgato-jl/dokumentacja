9486f9e7482a0613ef0d0fa1b5a06abc64f92592
elgato-jl <83208436+elgato-jl@users.noreply.github.com>
2021-04-30 11:35:00 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,6 +23,9 @@ const IndexPage = () => (
       <Link to="/page-2/">Go to page 2</Link> <br />
       <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
     </p>
+  <p>
+  eqweqweqwewqeqweqweqweqwe
+  </p>
   </Layout>
 )
 
